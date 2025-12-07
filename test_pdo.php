b6c24df43e22a76d3f2733ee4b3af7b7dca0cbd1
bintou05 <bintoubarry3105@gmail.com>
2025-12-07 11:42:35 +0000

    symfony_cours1

--- test_pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=gestion_employe', 'root', '');
-    echo "Connexion PDO MySQL réussie !";
-} catch (PDOException $e) {
-    echo "Erreur PDO : " . $e->getMessage();
-}
